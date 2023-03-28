c17b6fb9291681c5f87db5eef3f9411ee6f2b69f
kirillprinc <kirill.pristanskov@mail.ru>
2023-03-28 17:05:28 +0500

    Module04 task1 and task2

--- src/ru/javalang/module04/tasks/t02/Main.java
+++ src/ru/javalang/module04/tasks/t02/Main.java
@@ -1,5 +1,7 @@
 package ru.javalang.module04.tasks.t02;
 
+import java.util.Scanner;
+
 /**
  * Выведите на экран сумму четных элементов массива.
  *
@@ -15,6 +17,24 @@ package ru.javalang.module04.tasks.t02;
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Введите кол-во элементов массива: ");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] mas = new int[n];
+        int sum = 0;
 
+        for (int i=0; i<n; i++)
+            {
+                System.out.println("Введите элемент массива: ");
+                mas[i] = scan.nextInt();
+                if (mas[i] % 2 == 0) {
+                    sum = sum + mas[i];
+                    }
+            }
+            if (sum==0)
+            {System.out.println("NO");}
+            else {
+                System.out.println("Сумма четных элементов ровна " + sum);}
+
+        }
     }
-}
