096493683648ecb7eb0be03aee1f503f0ce2fd9e
kirillprinc <kirill.pristanskov@mail.ru>
2023-03-28 18:34:23 +0500

    Module04 task3

--- src/ru/javalang/module04/tasks/t03/Main.java
+++ src/ru/javalang/module04/tasks/t03/Main.java
@@ -1,5 +1,7 @@
 package ru.javalang.module04.tasks.t03;
 
+import java.util.Scanner;
+
 /**
  * Развернуть массив в обратную сторону ("задом наперед").
  *
@@ -16,4 +18,26 @@ package ru.javalang.module04.tasks.t03;
  *  1 2 3 4 5 6	        6 5 4 3 2 1
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите кол-во элементов массива: ");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] mas = new int[n];
+        for (int i=0; i<n; i++)
+        {
+            System.out.println("Введите элемент массива: ");
+            mas[i] = scan.nextInt();
+            }
+        for (int i=0; i<n/2; i++)
+        {int b = mas[i];
+        mas[i]=mas[n-i-1];
+        mas[n-i-1]=b;}
+
+        for (int i=0; i<n; i++)
+        {
+            System.out.println("Элемент массива " + (i+1) + " " + mas[i]);
+                    }
+
+    }
+
 }
