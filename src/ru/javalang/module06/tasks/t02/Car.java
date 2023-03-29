d789ba977ca2232171104010f6a79f8ccb82eed7
kirillprinc <kirill.pristanskov@mail.ru>
2023-03-29 13:19:26 +0500

    Update

--- src/ru/javalang/module06/tasks/t02/Car.java
+++ src/ru/javalang/module06/tasks/t02/Car.java
@@ -5,4 +5,17 @@ package ru.javalang.module06.tasks.t02;
  */
 
 public class Car {
+    public String catCar="B";
+    public String Transmission="mechanical";
+    public String nomberCar="B123JK34";
+    public int massaCar=12;
+
+    public Car(String catCar, String transmission, String nomberCar, int massaCar) {
+        this.catCar = catCar;
+        Transmission = transmission;
+        this.nomberCar = nomberCar;
+        this.massaCar = massaCar;
+
+
+}
 }
