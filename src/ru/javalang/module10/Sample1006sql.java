dfeef558f8a790362079eb4cd2d6e46fb726633f
kirillprinc <kirill.pristanskov@mail.ru>
2023-03-30 13:09:29 +0500

    Update

--- src/ru/javalang/module10/Sample1006sql.java
+++ src/ru/javalang/module10/Sample1006sql.java
@@ -17,7 +17,7 @@ public class Sample1006sql {
 //            DriverManager.registerDriver(new org.sqlite.JDBC());
 
             //Установление соединения
-            connection = DriverManager.getConnection("jdbc:sqlite:car.s3db");
+            connection = DriverManager.getConnection("jdbc:sqlite:C:\\study\\car.s3db");
 //            connection = DriverManager.getConnection("jdbc:sqlite:C:\\test\\car.s3db");
 
             //Создание экземпляра для выполнения запросов (без параметров)
