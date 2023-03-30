7801ab2152d3bfaebba9c6bffb91f56dbab51c28
kirillprinc <kirill.pristanskov@mail.ru>
2023-03-30 12:48:55 +0500

    Update

--- src/ru/javalang/module10/tasks/t01/Main.java
+++ src/ru/javalang/module10/tasks/t01/Main.java
@@ -1,4 +1,24 @@
 package ru.javalang.module10.tasks.t01;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int z=0; //Переменная для результата
+        System.out.println("Vvedite delimoe chislo:");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        System.out.println("Vvedite delitel: ");
+        int b = scan.nextInt();
+
+        try
+            {z=n/b;
+            } catch (ArithmeticException e)
+                {
+                    System.out.println("Chislo b rovno 0, delit na 0 nelzya");
+                                    }
+        if (b!=0){System.out.println("Rezultat deleniya raven: "+z);}
+        System.out.println("Programma vipolnena!");
+    }
 }
