5cfb925354e3609264521d648ae4a0fc9e79f02a
kirillprinc <kirill.pristanskov@mail.ru>
2023-03-30 18:11:03 +0500

    Update

--- src/ru/javalang/module11/tasks/t01/Main.java
+++ src/ru/javalang/module11/tasks/t01/Main.java
@@ -1,4 +1,24 @@
 package ru.javalang.module11.tasks.t01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader n = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int z = 0;
+        String d;
+        while ((d=n.readLine())!=null)
+            {int b = Integer.parseInt(d);
+        z += b;}
+
+        out.println(z);
+        out.flush();
+
+
+    }
 }
