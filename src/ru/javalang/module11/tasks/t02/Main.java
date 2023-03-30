47e9046c487acbdb18c9d9ddf950b111c2a1d07f
kirillprinc <kirill.pristanskov@mail.ru>
2023-03-30 23:42:14 +0500

    Update

--- src/ru/javalang/module11/tasks/t02/Main.java
+++ src/ru/javalang/module11/tasks/t02/Main.java
@@ -1,4 +1,21 @@
 package ru.javalang.module11.tasks.t02;
 
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        int r = 0;
+        int[] values = new int[5];
+        BufferedReader in = new BufferedReader(new
+                FileReader("C:\\study\\module11t2.txt"));
+        FileWriter write = new FileWriter("C:\\study\\module11t02.txt");
+        for (int i = 0; i < values.length; i++) {
+            values[i] = Integer.parseInt(in.readLine());
+        r+= values[i];
+        }
+        write.write(r);
+        System.out.println("Rezultat: " + r);
+        in.close();
+    }
+
 }
--- src/ru/javalang/module11/tasks/t03/Main.java
+++ src/ru/javalang/module11/tasks/t03/Main.java
@@ -1,4 +1,17 @@
 package ru.javalang.module11.tasks.t03;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Main {
+    public static void main(String[] args) {
+        User user = new User();
+        user.firstName="POP";
+        user.age=25;
+
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("C:\\study\\User.ser")))
+        {out.writeObject(user);}
+        catch (IOException e){System.out.println("Ошибка: "+e.getMessage());}
+    }
 }
