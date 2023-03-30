d0565f48fc5b6ca51967b9bf72b02386bbfc77f3
kirillprinc <kirill.pristanskov@mail.ru>
2023-03-30 14:06:15 +0500

    Update

--- src/ru/javalang/module10/tasks/t02/Main.java
+++ src/ru/javalang/module10/tasks/t02/Main.java
@@ -1,4 +1,23 @@
 package ru.javalang.module10.tasks.t02;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) {
+        try
+        {
+            FileInputStream test = new FileInputStream("C:\\study\\text.txt");
+            while (test.available()>0){int t = test.read();
+                System.out.println( (char) t + " ");}
+
+        }
+            catch (FileNotFoundException e)
+                {
+                    System.out.println("Voznikla problema: "+e);
+                                    } catch (IOException e) {
+            e.printStackTrace();
+            }
+    }
 }
